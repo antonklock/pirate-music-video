7b1563bbcd38b2ceee1a36da2d73f512492d9339
Anton Klock <anton@klockworks.se>
2024-02-25 19:12:18 +0100

    adding animated sprite test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,22 @@
 		}
 
 		const app = new PIXI.Application({ width: 256, height: 256, backgroundColor: 0x1099bb });
+		let sprite = PIXI.Sprite.from('/dcop_logo_01.png');
+		sprite.anchor.set(0.5);
+		sprite.y = app.renderer.height / 2;
+		sprite.scale = { x: 0.15, y: 0.15 };
+
+		app.stage.addChild(sprite);
+
+		let elapsed = 0.0;
+		app.ticker.add((delta) => {
+			// Add the time to our total elapsed time
+			elapsed += delta;
+			// Update the sprite's X position based on the cosine of our elapsed time.  We divide
+			// by 50 to slow the animation down a bit...
+			sprite.x = 125.0 + Math.cos(elapsed / 25.0) * 50.0;
+		});
+
 		container.appendChild(app.view as HTMLCanvasElement);
 	});
 </script>
